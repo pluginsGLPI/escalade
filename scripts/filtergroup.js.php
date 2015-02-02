cfe69961e652f0cebec8fe0339132675c8d2fdfe
Emmanuel Haguet <ehaguet@teclib.com>
2015-02-02 17:58:54 +0100

    Big refactoring of scripts/filtergroup.js.php

--- scripts/filtergroup.js.php
+++ scripts/filtergroup.js.php
@@ -5,91 +5,107 @@ include ("../../../inc/includes.php");
 header("Content-type: application/javascript");
 
 $JS = <<<JAVASCRIPT
+if (location.pathname.indexOf('ticket.form.php') == 0) {
+   exit;
+}
+
+// only in ticket form
+
+var url = '{$CFG_GLPI['root_doc']}/plugins/escalade/ajax/group_values.php';
+var tickets_id = getUrlParameter('id');
+
+function getValueOfSelectName(str) {
+   console.log("getValueOfSelectName("+str+")"); //DEBUG
+   console.log("select[name*='" + str + "']");
+   if ($("select[name*='" + str + "']").length > 0) {
+      return $("select[name*='" + str + "']")[0].attributes.getNamedItem('id').value;
+   } else {
+      console.log("Non trouvé");
+      return false;
+   }
+}
+
+//replace groups select by ajax response
+function remplaceGroupSelectByAjaxResponse(id, options) {
+   //var el = $("#" + id);
+   
+   var el = document.getElementById(id);
+   el.outerHTML = el.outerHTML.replace(el.innerHTML + '',options + '');
+}
+
 $(document).ready(function() {
-   // only in ticket form
-   if (location.pathname.indexOf('ticket.form.php') > 0) {
+   console.log("READY");
 
-      var tickets_id = getUrlParameter('id');
+   if (tickets_id == undefined) {
+      // -----------------------
+      // ---- Create Ticket ---- 
+      // -----------------------
 
-      if (tickets_id == undefined) {
-         // -----------------------
-         // ---- Create Ticket ---- 
-         // -----------------------
+     //perform an ajax request to get the new options for the group list
+      $.ajax({
+         url: url,
+         data: {'ticket_id': 0},
+         success: function(response, opts) {
 
-        //perform an ajax request to get the new options for the group list
-         $.ajax({
-            url: '{$CFG_GLPI['root_doc']}/plugins/escalade/ajax/group_values.php',
-            data: {
-               'ticket_id': 0
-            },
-            success: function(response, opts) {
-               var options = response.responseText;
+            setTimeout(function() {
+               
+               /*
+               var selectvalue = $("*[name='_groups_id_assign']")[0].value;
+               var id = $("*[name='_groups_id_assign']")[0].id; //"dropdown__groups_id_assign1186369991"
+               
+               $('#dropdown__groups_id_assign1186369991').select2(({ajax: {
+                  url: url}})
+               );
+               */
+               
+               /*
+               var assign_select_dom_id = getValueOfSelectName("_groups_id_assign");
+               remplaceGroupSelectByAjaxResponse(assign_select_dom_id, response.responseText);
+               */
+            }, 300);
+         },
+         fail: function(response, opts) {
+            console.log('server-side failure with status code ' + response.status);
+         }
+      });
 
-               setTimeout(function() {  
-                  //Tested with name=type : --Emmanuel
-	               var assign_select_dom_id = $("select[name=_groups_id_assign]")[0].attributes.getNamedItem('id').value; 
+   } else {
+      // -----------------------
+      // ---- Update Ticket ----
+      // -----------------------
 
-	               //replace groups select by AJAX response
-	               var el = document.getElementById(assign_select_dom_id);
-	               el.outerHTML = el.outerHTML.replace(el.innerHTML + '', options + '');
-	            }, 200);
-            },
-            fail: function(response, opts) {
-               console.log('server-side failure with status code ' + response.status);
-            }
-         });
+      Ext.Ajax.on('requestcomplete', function(conn, response, option) {
+      	//trigger the filter only on actor(group) selected
+         if (option.url.indexOf('dropdownItilActors.php') > 0 
+         	&& option.params.indexOf("group") > 0
+               && option.params.indexOf("assign") > 0
+            ) {
 
-      } else {
-         // -----------------------
-         // ---- Update Ticket ----
-         // -----------------------
+ 				//delay the execution (ajax requestcomplete event fired before dom loading)
+            setTimeout(function() {
 
-         //get id of itilactor select
-         //selector tested  --Emmanuel
-         var actor_select_dom_id = $("select[name*='_itil_assign[_type]']")[0].attributes.getNamedItem('id').value;
+            	$.ajax({
+	               url: url,
+	               data: {'ticket_id': tickets_id},
+	               success: function(response, opts) {
 
+	                  var assign_select_dom_id = getValueOfSelectName("_itil_assign[groups_id]");
 
-         Ext.Ajax.on('requestcomplete', function(conn, response, option) {
-         	//trigger the filter only on actor(group) selected
-            if (option.url.indexOf('dropdownItilActors.php') > 0 
-            	&& (
-                  option.params.indexOf("group") > 0
-                  && option.params.indexOf("assign") > 0
-               )) {
+	                  var nb_id = assign_select_dom_id.replace("dropdown__itil_assign[groups_id]", "");
 
-	 				//delay the execution (ajax requestcomplete event fired before dom loading)
-               setTimeout( function() {
+	                  //remove search input (only in GLPI AJAX mode)
+	                  $("#search_"+nb_id).remove();
 
-               	$.ajax({
-		               url: '{$CFG_GLPI['root_doc']}/plugins/escalade/ajax/group_values.php',
-		               data: {
-		                  'ticket_id': tickets_id
-		               },
-		               success: function(response, opts) {
-		                  var options = response.responseText;
+	                  remplaceGroupSelectByAjaxResponse(assign_select_dom_id, response.responseText);
+	               }
+	            });
 
-		                  var assign_select_dom_id = $("select[name*='_itil_assign[groups_id]']")[0].attributes.getNamedItem('id').value;
+            }, 300);
+			}
 
-		                  var nb_id = assign_select_dom_id.replace("dropdown__itil_assign[groups_id]", "");
+      }, this);
 
-		                  //remove search input (only in GLPI AJAX mode)
-		                  if ($("#search_"+nb_id).length != 0) {
-		                  	$("#search_"+nb_id).remove();
-		                  }
-
-		                  //replace groups select by ajax response
-               			var el = document.getElementById(assign_select_dom_id);
-			               el.outerHTML = el.outerHTML.replace(el.innerHTML + '',options + '');
-		               }
-		            });
-
-               }, 300);
-				}
-
-         }, this);
-
-         
-      }
+      
    }
 });
 JAVASCRIPT;
