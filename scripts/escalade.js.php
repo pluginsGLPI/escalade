178c66a43f1abfb0c78c9cbb666c6222fa191974
Emmanuel Haguet <ehaguet@teclib.com>
2015-02-04 15:45:04 +0100

    Port /scripts/escalade.js.php

--- scripts/escalade.js.php
+++ scripts/escalade.js.php
@@ -11,15 +11,9 @@ if ($_SESSION['glpiactiveprofile']['interface'] == "central") {
    ) {
    
    $locale_actor = __('Actor');
+   $locale_group_view = __('Group View');
 
    $JS = <<<JAVASCRIPT
-   // get central list for plugin and insert in group tab
-   function getSelectorCentralList() {
-      var selector = "div[id^=Central][id$=2] .tab_cadre_central > tbody > tr > td:nth(2)";
-      selector += ", div.alltab:contains('groupe') + .tab_cadre_central > tbody > tr > td:nth(2)";
-      return selector;
-   }
-   
    function ticketEscalation() {
       var url = '../plugins/escalade/ajax/history.php';
       
@@ -43,6 +37,54 @@ if ($_SESSION['glpiactiveprofile']['interface'] == "central") {
       });
    }
    
+   function doOnCentralPage() {
+      $(document).ajaxSend(function(event, jqxhr, option) {
+         //intercept ajax load of group tab
+         if (option.url.indexOf('common.tabs.php') > 0 
+            /* && (
+               option.url.indexOf("Central$2") > 0 //TODO : option.params
+               || option.url.indexOf("-1") > 0 //option.params
+            )*/) {
+            console.log(option.url);
+            //delay the execution (ajax requestcomplete event fired before dom loading)
+            setTimeout(function () {
+               var test_span = "<span id='escalade_block"+suffix+"'></span>";
+               
+               var suffix = "";
+               var selector = "#ui-tabs-2 .tab_cadre_central tr td:nth-child(2)" +
+                  ", .alltab:contains('$locale_group_view') + .tab_cadre_central > tbody > tr > td:nth-child(2)";
+               
+               console.log($(selector)); //arf !
+               console.log(selector);
+               // get central list for plugin and insert in group tab
+               $(selector).each(function(){
+                  console.log("in each");
+                  //console.log(element);
+                  //console.log($( this ).text());
+                  
+                  console.log(this.innerHTML);
+                  if (this.innerHTML.indexOf('escalade_block') < 0) {
+                     
+                     if (option.url.indexOf("-1") > 0) { //option.params
+                        suffix = "_all";
+                     }
+                     
+                     //prepare a span element to load new elements
+                     //el.insertHtml('afterBegin', test_span);
+                     $(this).after(test_span);
+                     
+                     //ajax request
+                     selectorbis = "#escalade_block"+suffix;
+                     $(selectorbis).load('../plugins/escalade/ajax/central.php');
+                     $(document).load('../plugins/escalade/ajax/central.php'); //script yes
+                     console.log("after request ajax");
+                  }
+               });
+            }, 300);
+         }
+      });
+   };
+   
    $(document).ready(function() {
 
       // only in ticket form
@@ -60,41 +102,8 @@ if ($_SESSION['glpiactiveprofile']['interface'] == "central") {
 
       // only on central page
       if (location.pathname.indexOf('central.php') > 0) {
-         Ext.Ajax.on('requestcomplete', function(conn, response, option) {
-            
-            //intercept ajax load of group tab 
-            if (option.url.indexOf('common.tabs.php') > 0 
-               && (
-                  option.params.indexOf("Central$2") > 0
-                  || option.params.indexOf("-1") > 0
-               )) {
-
-               //delay the execution (ajax requestcomplete event fired before dom loading)
-               setTimeout( function () {
-                  //if loading of new element already done, return;
-                  //if (Ext.select("#escalade_block").elements.length > 0) return;
-
-                  suffix = "";
-                  
-                  var selector = getSelectorCentralList();
-                  
-                  Ext.select(selector).each(function(el) {
-                     if (el.dom.innerHTML.indexOf('escalade_block') < 0) {
-
-                        if (option.params.indexOf("-1") > 0) {
-                           suffix = "_all";
-                        }
-
-                        //prepare a span element to load new elements
-                        el.insertHtml('afterBegin', "<span id='escalade_block"+suffix+"'>test</span>");
-
-                        //load HTML in this Span (with execution on Javascript)
-                        $("#escalade_block"+suffix).load('../plugins/escalade/ajax/central.php');
-                     }
-                  });
-               }, 300);
-            }
-         }, this);
+         console.log("doOnCentralPage function");
+         doOnCentralPage();
       }
 
 
