fda61f8c72db4f7b22f5335dfbb76ae6b890d83f
Emmanuel Haguet <ehaguet@teclib.com>
2015-02-05 16:45:26 +0100

    change code for remove a ajaxComplete

--- scripts/escalade.js.php
+++ scripts/escalade.js.php
@@ -40,58 +40,41 @@ if ($_SESSION['glpiactiveprofile']['interface'] == "central") {
       });
    }
    
-   function doOnCentralPage() {
+   // only on central page
+   if (location.pathname.indexOf('central.php') > 0) {
       //intercept ajax load of group tab
-      $(document).ajaxComplete(function(event, jqxhr, option) {
-         
-         if (option.url == "../plugins/escalade/ajax/central.php") return;
+      $("*").on("tabsload", function() {
 
-         if (option.url.indexOf('common.tabs.php') > 0 
-            /* && (
-               option.url.indexOf("Central$2") > 0 //TODO : option.params
-               || option.url.indexOf("-1") > 0 //option.params
-            )*/) {
-            //delay the execution (ajax requestcomplete event fired before dom loading)
-            setTimeout(function () {
+         var suffix = "";
+         var selector = "#ui-tabs-2 .tab_cadre_central .top:last" +
+            ", .alltab:contains('$locale_group_view') + .tab_cadre_central .top:last";
+         console.log(selector);
+         
+         // get central list for plugin and insert in group tab
+         $(selector).each(function(){
+            
+            if (this.innerHTML.indexOf('escalade_block') < 0) {
                
-               var suffix = "";
-               var selector = "#ui-tabs-2 .tab_cadre_central .top:last" +
-                  ", .alltab:contains('$locale_group_view') + .tab_cadre_central .top:last";
-               //console.log(selector);
+               //if (option.url.indexOf("-1") > 0) { //option.params
+               //   suffix = "_all";
+               //}
                
-               // get central list for plugin and insert in group tab
-               $(selector).each(function(){
-                  
-                  if (this.innerHTML.indexOf('escalade_block') < 0) {
-                     
-                     if (option.url.indexOf("-1") > 0) { //option.params
-                        suffix = "_all";
-                     }
-                     
-                     //prepare a span element to load new elements
-                     $(this).prepend("<span id='escalade_block"+suffix+"'>test</span>");
-                     
-                     //ajax request
-                     selectorbis = "#escalade_block"+suffix;
-                     //console.log(selectorbis);
-                     $(selectorbis).load('../plugins/escalade/ajax/central.php');
-                  }
-               });
-            }, 300);
-         }
+               //prepare a span element to load new elements
+               $(this).prepend("<span id='escalade_block"+suffix+"'>test</span>");
+               
+               //ajax request
+               selectorbis = "#escalade_block"+suffix;
+               //console.log(selectorbis);
+               $(selectorbis).load('../plugins/escalade/ajax/central.php');
+            }
+         });
       });
    }
    
-   // only on central page
-   if (location.pathname.indexOf('central.php') > 0) {
-      doOnCentralPage();
-   }
+   // only in ticket form
+   if (location.pathname.indexOf('ticket.form.php') > 0) {
+      $('#tabspanel + div.ui-tabs').on("tabsload", function( event, ui ) {
    
-   $('#tabspanel + div.ui-tabs').on("tabsload", function( event, ui ) {
-
-      // only in ticket form
-      if (location.pathname.indexOf('ticket.form.php') > 0) {
-      
          tickets_id = getUrlParameter('id');
          
          //only in edit form
@@ -100,9 +83,8 @@ if ($_SESSION['glpiactiveprofile']['interface'] == "central") {
          setTimeout(function() {
             ticketEscalation();
          }, 300);
-      }
-
-   });
+      });
+   }
 JAVASCRIPT;
       echo $JS;
    }
