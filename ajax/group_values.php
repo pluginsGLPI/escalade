345272c9c7ac6cdec46040e1611a0d219202dbc9
Emmanuel Haguet <ehaguet@teclib.com>
2015-02-05 14:14:17 +0100

    fix filter

--- ajax/group_values.php
+++ ajax/group_values.php
@@ -20,8 +20,9 @@ if (count($groups_id_filtred) > 0) {
    $condition = " id IN ($newarray)";
    
 } else {
-   $condition = "1=1";
+   $condition = "1=0";
 }
+Toolbox::logDebug($condition);
 
 $rand = mt_rand();
 $_SESSION['glpicondition'][$rand] = $condition;
