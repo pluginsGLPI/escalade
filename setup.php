4275b84773cdb7e9d40a92bbdaba7c4349593370
Alexandre Delaunay <adelaunay@teclib.com>
2015-08-24 09:31:27 +0200

    checks for 0.90 and clean

--- setup.php
+++ setup.php
@@ -9,12 +9,14 @@ function plugin_init_escalade() {
    $plugin = new Plugin();
    if (isset($_SESSION['glpiID']) 
       && $plugin->isInstalled('escalade') && $plugin->isActivated('escalade')) {
+
       //load config in session
       if (TableExists("glpi_plugin_escalade_configs")) {
          PluginEscaladeConfig::loadInSession();
 
          // == Load js scripts ==
          if (isset($_SESSION['plugins']['escalade']['config'])) {
+            $escalade_config = $_SESSION['plugins']['escalade']['config'];
             
             $PLUGIN_HOOKS['add_javascript']['escalade'][] = 'scripts/function.js';
 
@@ -22,8 +24,8 @@ function plugin_init_escalade() {
                || strpos($_SERVER['REQUEST_URI'], "central.php") !== false) {
 
                //history and climb feature
-               if ($_SESSION['plugins']['escalade']['config']['show_history'] 
-                  && $_SESSION['plugins']['escalade']['config']['remove_group']) {
+               if ($escalade_config['show_history'] 
+                  && $escalade_config['remove_group']) {
                   $PLUGIN_HOOKS['add_javascript']['escalade'][] = 'scripts/escalade.js.php';
                }
             }
@@ -33,12 +35,12 @@ function plugin_init_escalade() {
                $PLUGIN_HOOKS['add_javascript']['escalade'][] = 'scripts/remove_btn.js.php';
 
                //clone ticket feature
-               if ($_SESSION['plugins']['escalade']['config']['cloneandlink_ticket']) {
+               if ($escalade_config['cloneandlink_ticket']) {
                   $PLUGIN_HOOKS['add_javascript']['escalade'][] = 'scripts/cloneandlink_ticket.js.php';
                }
                
                //filter group feature
-               if ($_SESSION['plugins']['escalade']['config']['use_filter_assign_group']) {
+               if ($escalade_config['use_filter_assign_group']) {
                   $PLUGIN_HOOKS['add_javascript']['escalade'][] = 'scripts/filtergroup.js.php';
                }
             }
@@ -47,28 +49,28 @@ function plugin_init_escalade() {
          }
       }
 
-      $PLUGIN_HOOKS['add_css']            ['escalade'][]          = 'style.css';
+      $PLUGIN_HOOKS['add_css']['escalade'][]= 'style.css';
 
       // == Ticket modifications
-      $PLUGIN_HOOKS['item_update']        ['escalade']            = array(
+      $PLUGIN_HOOKS['item_update']['escalade']= array(
          'Ticket'       => 'plugin_escalade_item_update',
       );
-      $PLUGIN_HOOKS['item_add']           ['escalade']            = array(
+      $PLUGIN_HOOKS['item_add']['escalade'] = array(
          'Group_Ticket' => 'plugin_escalade_item_add_group_ticket',
          'Ticket_User'  => 'plugin_escalade_item_add_user',
          'Ticket'       => 'plugin_escalade_item_add_ticket',
       );
-      $PLUGIN_HOOKS['pre_item_add']       ['escalade']            = array(
+      $PLUGIN_HOOKS['pre_item_add']['escalade'] = array(
          'Group_Ticket' => 'plugin_escalade_pre_item_add_group_ticket',
          'Ticket'       => 'plugin_escalade_pre_item_add_ticket',
       );
       $PLUGIN_HOOKS['post_prepareadd']['escalade'] = array(
-         'Ticket'    => 'plugin_escalade_post_prepareadd_ticket',
+         'Ticket'       => 'plugin_escalade_post_prepareadd_ticket',
       );
 
       // == Interface links ==
       if (Session::haveRight('config', UPDATE)) {
-         $PLUGIN_HOOKS['config_page']     ['escalade']            = 'front/config.form.php';
+         $PLUGIN_HOOKS['config_page']['escalade'] = 'front/config.form.php';
       }
 
 
@@ -79,10 +81,9 @@ function plugin_init_escalade() {
 function plugin_version_escalade() {
    return array(
          'name'           => __("Escalation", "escalade"),
-         'version'        => "0.85-1.1",
-         'author'         => "<a href='http://www.teclib.com'>Teclib'</a> &
-                              <a href='http://www.lefigaro.fr/'>LE FIGARO</a>",
-         'homepage'       => "https://forge.indepnet.net/projects/escalade",
+         'version'        => "0.90-1.0",
+         'author'         => "<a href='http://www.teclib.com'>Teclib'</a>",
+         'homepage'       => "https://github.com/pluginsGLPI/escalade",
          'license'        => 'GPLv2+',
          'minGlpiVersion' => "0.85",
    );
@@ -91,8 +92,8 @@ function plugin_version_escalade() {
 // Optional : check prerequisites before install : may print errors or add to message after redirect
 function plugin_escalade_check_prerequisites() {
 
-   if (version_compare(GLPI_VERSION,'0.85','lt') || version_compare(GLPI_VERSION,'0.86','ge')) {
-      echo "This plugin requires GLPI >= 0.85 and GLPI < 0.86";
+   if (version_compare(GLPI_VERSION,'0.85','lt')) {
+      echo "This plugin requires GLPI >= 0.85";
    } else {
       return true;
    }
