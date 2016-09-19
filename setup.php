f1ef6912e206e76af4a35676cfe1051950c8afeb
Johan Cwiklinski <jcwiklinski@teclib.com>
2016-09-19 12:44:55 +0200

    Bump version. 1.2 has already been released.

--- setup.php
+++ setup.php
@@ -97,7 +97,7 @@ function plugin_init_escalade() {
 function plugin_version_escalade() {
    return array(
          'name'           => __("Escalation", "escalade"),
-         'version'        => "0.90-1.2",
+         'version'        => "0.90-1.3",
          'author'         => "<a href='http://www.teclib.com'>Teclib'</a>",
          'homepage'       => "https://github.com/pluginsGLPI/escalade",
          'license'        => 'GPLv2+',
