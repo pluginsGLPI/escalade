7308b27d8088ec3cb86e19e33c9825cd6dfb2685
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2017-02-21 16:56:25 +0100

    add escalated groups in planning recall

--- inc/notification.class.php
+++ inc/notification.class.php
@@ -12,9 +12,13 @@ class PluginEscaladeNotification {
    const NTRGT_TICKET_TECH_GROUP_MANAGER      = 357959;
    const NTRGT_TASK_GROUP                     = 357960;
 
+   const NTRGT_TICKET_ESCALADE_GROUP          = 457951;
+   const NTRGT_TICKET_ESCALADE_GROUP_MANAGER  = 457952;
+
    static function addTargets(NotificationTarget $target) {
+      // only for Planning recall
       if ($target instanceof NotificationTargetPlanningRecall) {
-         // add new targets to planning recall notification
+         // add new native targets
          $target->addTarget(self::NTRGT_TICKET_REQUESTER_USER,
             __('Requester user of the ticket', 'escalade'));
          $target->addTarget(self::NTRGT_TICKET_REQUESTER_GROUP,
@@ -36,6 +40,13 @@ class PluginEscaladeNotification {
          $target->addTarget(self::NTRGT_TASK_GROUP,
             __('Group in charge of the task'));
 
+
+         // add plugins targets
+         $target->addTarget(self::NTRGT_TICKET_ESCALADE_GROUP,
+            __('Group escalated in the ticket', 'escalade'));
+         $target->addTarget(self::NTRGT_TICKET_ESCALADE_GROUP_MANAGER,
+            __('Manager of the group escalated in the ticket', 'escalade'));
+
          // change label for this core target to avoid confusion with NTRGT_TICKET_REQUESTER_USER
          $target->addTarget(Notification::AUTHOR,
             __('Requester user of the task/reminder', 'escalade'));
@@ -52,37 +63,43 @@ class PluginEscaladeNotification {
             $ticket->getFromDB($item->getField('tickets_id'));
 
             switch ($target->data['items_id']) {
+               // group's users
                case self::NTRGT_TICKET_REQUESTER_GROUP:
-                  $manager = 0;
-               case self::NTRGT_TICKET_REQUESTER_GROUP_MANAGER:
-                  if (!isset($manager)) {
-                     $manager = 1;
-                  }
-                  self::addGroupsOfTicket($ticket->getID(), $manager, CommonITILActor::REQUESTER);
-                  break;
-
+                  $group_type = CommonITILActor::REQUESTER;
                case self::NTRGT_TICKET_WATCH_GROUP:
-                  $manager = 0;
-               case self::NTRGT_TICKET_WATCH_GROUP_MANAGER:
-                  if (!isset($manager)) {
-                     $manager = 1;
+                  if (!isset($group_type)) {
+                     $group_type = CommonITILActor::OBSERVER;
                   }
-                  self::addGroupsOfTicket($ticket->getID(), $manager, CommonITILActor::OBSERVER);
-                  break;
-
                case self::NTRGT_TICKET_TECH_GROUP:
                   $manager = 0;
+
+               // manager of group's users
+               case self::NTRGT_TICKET_REQUESTER_GROUP_MANAGER:
+                  if (!isset($group_type)) {
+                     $group_type = CommonITILActor::REQUESTER;
+                  }
+               case self::NTRGT_TICKET_WATCH_GROUP_MANAGER:
+                  if (!isset($group_type)) {
+                     $group_type = CommonITILActor::OBSERVER;
+                  }
                case self::NTRGT_TICKET_TECH_GROUP_MANAGER:
                   if (!isset($manager)) {
                      $manager = 1;
                   }
-                  self::addGroupsOfTicket($ticket->getID(), $manager, CommonITILActor::ASSIGN);
+                  if (!isset($group_type)) {
+                     $group_type = CommonITILActor::ASSIGN;
+                  }
+
+                  self::addGroupsOfTicket($ticket->getID(), $manager, $group_type);
                   break;
 
+               // users
                case self::NTRGT_TICKET_REQUESTER_USER:
                   $user_type = CommonITILActor::REQUESTER;
                case self::NTRGT_TICKET_WATCH_USER:
-                  $user_type = CommonITILActor::OBSERVER;
+                  if (!isset($user_type)) {
+                     $user_type = CommonITILActor::OBSERVER;
+                  }
                case self::NTRGT_TICKET_TECH_USER:
                   if (!isset($user_type)) {
                      $user_type = CommonITILActor::ASSIGN;
@@ -90,25 +107,55 @@ class PluginEscaladeNotification {
                   self::addUsersOfTicket($ticket->getID(), $user_type);
                   break;
 
+               // task group
                case self::NTRGT_TASK_GROUP:
                   $target->getAddressesByGroup(0, $item->getField('groups_id_tech'));
+                  break;
+
+               // escalation groups
+               case self::NTRGT_TICKET_ESCALADE_GROUP:
+                  $manager = 0;
+               case self::NTRGT_TICKET_ESCALADE_GROUP_MANAGER:
+                  if (!isset($manager)) {
+                     $manager = 1;
+                  }
+                  $history = new PluginEscaladeHistory;
+                  foreach($history->find("`tickets_id` = ".$ticket->getID()) as $found_history) {
+                     $target->getAddressesByGroup($manager, $found_history['groups_id']);
+                  }
                   break;
             }
          }
       }
    }
 
-   static function addGroupsOfTicket($tickets_id = 0, $manager = 0, $type = CommonITILActor::REQUESTER) {
+   /**
+    * Add all group's users for a ticket and a type of actors
+    *
+    * @param integer $tickets_id The ticket's identifier
+    * @param integer $manager    0 all users, 1 only supervisors, 2 all users without supervisors
+    * @param integer $group_type @see CommonITILActor
+    *
+    * @return  nothing
+    */
+   static function addGroupsOfTicket($tickets_id = 0, $manager = 0, $group_type = CommonITILActor::REQUESTER) {
       $group_ticket = new Group_Ticket;
-      foreach($group_ticket->find("`tickets_id` = $tickets_id AND `type` = $type") as $current) {
+      foreach($group_ticket->find("`tickets_id` = $tickets_id AND `type` = $group_type") as $current) {
          $target->getAddressesByGroup($manager, $current['groups_id']);
       }
    }
 
-   static function addUsersOfTicket($tickets_id = 0, $type = CommonITILActor::REQUESTER) {
+   /**
+    * Add all users for a ticket and a type of actors
+    * @param integer $tickets_id The ticket's identifier
+    * @param integer $user_type @see CommonITILActor
+    *
+    * @return  nothing
+    */
+   static function addUsersOfTicket($tickets_id = 0, $user_type = CommonITILActor::REQUESTER) {
       $ticket_user = new Ticket_User;
       $user        = new User;
-      foreach($ticket_user->find("`type` = $type
+      foreach($ticket_user->find("`type` = $user_type
                                   AND `tickets_id` = $tickets_id") as $current) {
          if ($user->getFromDB($current['users_id'])) {
             $target->addToAddressesList(['language' => $user->getField('language'),
