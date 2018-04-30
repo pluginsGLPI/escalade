462f1075bd71fb078b2cc38966da68689580eca9
Cédric Anne <cedric.anne@gmail.com>
2018-04-30 17:18:52 +0200

    Fix deprecated calls

--- inc/config.class.php
+++ inc/config.class.php
@@ -280,7 +280,7 @@ class PluginEscaladeConfig extends CommonDBTM {
       if (isset($config->fields['use_filter_assign_group'])
           && $config->fields['use_filter_assign_group']) {
          $user = new PluginEscaladeUser();
-         if ($user->getFromDBByQuery("WHERE users_id = '".$_SESSION['glpiID']."'")) {
+         if ($user->getFromDBByCrit(['users_id' => $_SESSION['glpiID']])) {
             $config->fields['use_filter_assign_group'] = $user->fields['use_filter_assign_group'];
          }
       }
