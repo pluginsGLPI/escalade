d5afa8b132d176fc83aced6fcbe603393bc25313
François Legastelois <flegastelois@teclib.com>
2021-12-30 09:46:49 +0100

    feat: bump to 2.7.2

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define ('PLUGIN_ESCALADE_VERSION', '2.7.1');
+define ('PLUGIN_ESCALADE_VERSION', '2.7.2');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_ESCALADE_MIN_GLPI", "9.5");
