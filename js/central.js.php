92923289f2c3f7c5332a731f47ec94630fc5f3de
stonebuzz <skita@teclib.com>
2022-10-20 14:48:02 +0200

    fix random position

--- js/central.js.php
+++ js/central.js.php
@@ -49,23 +49,17 @@ if ($_SESSION['glpiactiveprofile']['interface'] == "central"
             if ($('.nav-link.active:contains($locale_group_view)').length == 0) {
                return;
             }
-
-            // get central list for plugin and insert in group tab
             var container = $(".masonry_grid");
-            //ajax request
             $.ajax({ type: "GET",
                url: CFG_GLPI.root_doc+"/"+GLPI_PLUGINS_PATH.escalade+'/ajax/central.php',
                async: false,
                success : function(text)
                {
                   container.append(text);
-                  window.msnry = new Masonry('.masonry_grid', {
-                  "percentPosition": true,
-                  "horizontalOrder": true,
-               });
+                  window.msnry = new Masonry('.masonry_grid');
                }
             });
-          }, 10);
+          }, 100);
       });
    });
 
