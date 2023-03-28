f39db8ac9fc24cf97e6c2f3cae847d9ce6c6ec72
THIVET Simon <simon.thivet@infotel.com>
2023-03-28 16:23:23 +0200

    Minor fix

--- inc/config.class.php
+++ inc/config.class.php
@@ -31,7 +31,12 @@
 class PluginEscaladeConfig extends CommonDBTM {
    static $rightname  = 'config';
 
-   static function getTypeName($nb = 0) {
+   public function __construct()
+   {
+       $this->getFromDB(1);
+   }
+
+    static function getTypeName($nb = 0) {
       return __("Configuration Escalade plugin", "escalade");
    }
 
