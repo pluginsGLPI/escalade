ac57edc20a167735eb136a0cb34b2b373324aebf
Sthivet <121936216+Sthivet@users.noreply.github.com>
2024-02-21 11:53:36 +0100

    Fix group dropdown in escalate form (#163)

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -918,19 +918,22 @@ class PluginEscaladeTicket {
         foreach ($groups as $grp) {
             $assigned_groups[$grp['groups_id']] = $grp['groups_id'];
         }
-
+        $config = new PluginEscaladeConfig();
+        $config->getFromDB(1);
         $PluginEscaladeGroup_Group = new PluginEscaladeGroup_Group();
         $groups_id_filtered = array_keys($PluginEscaladeGroup_Group->getGroups($tickets_id));
         $groups_id_filtered = empty($groups_id_filtered) ? [-1] : $groups_id_filtered;
-
+        $condition = [
+            'is_assign' => 1
+        ];
+        if ($config->fields['use_filter_assign_group']) {
+            $condition['id'] = $groups_id_filtered;
+        }
         TemplateRenderer::getInstance()->display('@escalade/escalade_form.html.twig', [
             'action'          => PLUGIN_ESCALADE_WEBDIR . '/front/ticket.form.php',
             'ticket'          => $options['parent'],
             'assigned_groups' => $assigned_groups,
-            'condidition'     => [
-                'is_assign' => 1,
-                'id' => $groups_id_filtered,
-            ],
+            'condition'     => $condition,
         ]);
     }
 }
