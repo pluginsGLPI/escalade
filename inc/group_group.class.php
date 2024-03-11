fbc2819cf35844420fbf8c6d6374ebe7f78ea02b
stonebuzz <skita@teclib.com>
2024-03-11 15:45:07 +0100

    filtering group only if needed

--- inc/group_group.class.php
+++ inc/group_group.class.php
@@ -183,6 +183,10 @@ class PluginEscaladeGroup_Group extends CommonDBRelation {
          $groupname = $group_obj->fields['name'];
       }
 
+      if (count($groups) == 0) {
+         Group_User::getUserGroups($_SESSION['glpiID']);
+      }
+
       //sort by group name (and keep associative index)
       asort($groups);
 
