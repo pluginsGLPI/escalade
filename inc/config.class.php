54d70b0984fb9f38219d5071c553c94a77bb9248
RomainLvr <lecouvreurromain@gmail.com>
2024-06-11 14:11:43 +0200

    Deleting the link between a script and the field

--- inc/config.class.php
+++ inc/config.class.php
@@ -192,7 +192,6 @@ class PluginEscaladeConfig extends CommonDBTM
         echo __("Assign me as observer by default", "escalade") . "</label></td>";
         echo "<td>";
         Dropdown::showYesNo("assign_me_as_observer", $this->fields["assign_me_as_observer"], -1, [
-            'on_change' => 'hide_show_history(this.value)',
             'width' => '25%',
             'rand' => $rand,
         ]);
