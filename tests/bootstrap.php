becdf4648fa84a025592ac27b94c2d4978436cb5
Lainow <texamaxsam61@gmail.com>
2024-08-06 15:22:22 +0200

    Add suggestions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,10 +28,10 @@
  * -------------------------------------------------------------------------
  */
 
-global $CFG_GLPI;
+global $CFG_GLPI, $PLUGIN_HOOKS;
 
 define('GLPI_ROOT', __DIR__ . '/../../../');
-define('GLPI_LOG_DIR', __DIR__ . '/files/_logs');
+define('GLPI_LOG_DIR', GLPI_ROOT . '/files/_logs');
 
 define('TU_USER', 'glpi');
 define('TU_PASS', 'glpi');
