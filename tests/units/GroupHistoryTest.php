481af5c67698d72890a8a69dc2ba020283a46ee1
ArthurMinfotel <arthur.mallet@infotel.com>
2025-01-21 16:43:59 +0100

    license header

--- tests/units/GroupHistoryTest.php
+++ tests/units/GroupHistoryTest.php
@@ -1,10 +1,37 @@
 <?php
 
+/**
+ * -------------------------------------------------------------------------
+ * Escalade plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Escalade.
+ *
+ * Escalade is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Escalade is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Escalade. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2015-2023 by Escalade plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/escalade
+ * -------------------------------------------------------------------------
+ */
+
 namespace GlpiPlugin\Escalade\Tests\units;
 
 use GlpiPlugin\Escalade\Tests\EscaladeTestCase;
 use CommonITILActor;
-
 use PluginEscaladeConfig;
 
 final class GroupHistoryTest extends EscaladeTestCase
@@ -20,9 +47,7 @@ final class GroupHistoryTest extends EscaladeTestCase
         $this->assertGreaterThan(0, $conf['id']);
 
         // Update escalade config
-        $this->assertTrue($config->update([
-                'remove_group' => 1
-            ] + $conf));
+        $this->assertTrue($config->update(['remove_group' => 1] + $conf));
 
         PluginEscaladeConfig::loadInSession();
 
