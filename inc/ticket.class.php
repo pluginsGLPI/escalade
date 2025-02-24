8b2be5f4b26198220af711517852837e7dd54b89
MyuTsu <gaetan.langlois31@gmail.com>
2025-02-24 09:29:05 +0100

    add comment

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -408,6 +408,7 @@ class PluginEscaladeTicket
         if ($_SESSION['glpi_plugins']['escalade']['config']['remove_group'] == true) {
             self::removeAssignGroups($tickets_id, $groups_id);
         }
+        //remove old users if remove_tech is enabled
         self::removeAssignUsers($item);
     }
 
