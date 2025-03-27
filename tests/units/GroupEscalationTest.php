6fd0fd8e891439f8bf5ac0de28973cf7c27a26bf
MyuTsu <gaetan.langlois31@gmail.com>
2025-03-27 17:02:20 +0100

    add TU

--- tests/units/GroupEscalationTest.php
+++ tests/units/GroupEscalationTest.php
@@ -37,7 +37,7 @@ use PluginEscaladeConfig;
 
 final class GroupEscalationTest extends EscaladeTestCase
 {
-    public function testTechGroupAttribution()
+    public function testTechGroupAttributionUpdateTicket()
     {
         $this->login();
 
@@ -50,7 +50,7 @@ final class GroupEscalationTest extends EscaladeTestCase
         // Update escalade config
         $this->assertTrue($config->update([
             'use_assign_user_group'              => 1,
-            'use_assign_user_group_creation'     => 1,
+            'use_assign_user_group_creation'     => 0,
             'use_assign_user_group_modification' => 1,
             'remove_tech'                        => 1,
             'remove_group'                       => 1,
@@ -137,6 +137,99 @@ final class GroupEscalationTest extends EscaladeTestCase
         ]);
         // Check group assigned to the ticket
         $this->assertEquals($group2_id, $ticket_group2->fields['groups_id']);
+
+        $ticket_user = new \Ticket_User();
+        // Check user assigned to the ticket
+        $this->assertEquals(2, count($ticket_user->find(['tickets_id' => $t_id])));
+
+        // Check requester
+        $this->assertEquals(1, count($ticket_user->find(['tickets_id' => $t_id, 'users_id' => $user1->getID(), 'type' => CommonITILActor::REQUESTER])));
+
+        // Check assign
+        $this->assertEquals(1, count($ticket_user->find(['tickets_id' => $t_id, 'users_id' => $user2->getID(), 'type' => CommonITILActor::ASSIGN])));
+    }
+
+    public function testTechGroupAttributionAddTicket()
+    {
+        $this->login();
+
+        $config = new PluginEscaladeConfig();
+        $conf = $config->find();
+        $conf = reset($conf);
+        $config->getFromDB($conf['id']);
+        $this->assertGreaterThan(0, $conf['id']);
+
+        // Update escalade config
+        $this->assertTrue($config->update([
+            'use_assign_user_group'              => 1,
+            'use_assign_user_group_creation'     => 1,
+            'use_assign_user_group_modification' => 0,
+            'remove_tech'                        => 1,
+            'remove_group'                       => 1,
+        ] + $conf));
+
+        PluginEscaladeConfig::loadInSession();
+
+        $user1 = new \User();
+        $user1->getFromDBbyName('glpi');
+        $this->assertGreaterThan(0, $user1->getID());
+
+        $group1 = new \Group();
+        $group1_id = $group1->add(['name' => 'Group_1']);
+        $this->assertGreaterThan(0, $group1_id);
+
+        $user_group1 = new \Group_User();
+        $user_group1->add([
+            'users_id' => $user1->getID(),
+            'groups_id' => $group1->getID()
+        ]);
+        $this->assertGreaterThan(0, $user_group1->getID());
+
+        $user2 = new \User();
+        $user2->getFromDBbyName('tech');
+        $this->assertGreaterThan(0, $user2->getID());
+
+        $group2 = new \Group();
+        $group2_id = $group2->add(['name' => 'Group_2']);
+        $this->assertGreaterThan(0, $group2_id);
+
+        $user_group2 = new \Group_User();
+        $user_group2->add([
+            'users_id' => $user2->getID(),
+            'groups_id' => $group2->getID()
+        ]);
+        $this->assertGreaterThan(0, $user_group2->getID());
+
+        // Create ticket with technician
+        $ticket = new \Ticket();
+        $t_id = $ticket->add([
+            'name' => 'Assign Group Escalation Test',
+            'content' => '',
+            '_actors' => [
+                'requester' => [
+                    [
+                        'items_id' => $user1->getID(),
+                        'itemtype' => 'User'
+                    ]
+                ],
+                'assign' => [
+                    [
+                        'items_id' => $user2->getID(),
+                        'itemtype' => 'User'
+                    ]
+                ],
+            ]
+        ]);
+
+        // Check one group linked to the ticket
+        $ticket_group = new \Group_Ticket();
+        $this->assertEquals(1, count($ticket_group->find(['tickets_id' => $t_id])));
+
+        $ticket_group->getFromDBByCrit([
+            'tickets_id' => $t_id,
+        ]);
+        // Check group assigned to the ticket
+        $this->assertEquals($group2_id, $ticket_group->fields['groups_id']);
 
         $ticket_user = new \Ticket_User();
         // Check user assigned to the ticket
