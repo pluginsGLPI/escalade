e15df64e805cf4cba81b579a239ad7a2638e3253
Rom1-B <rom1.biot@gmail.com>
2025-06-05 11:44:45 +0200

    fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,11 +28,59 @@
  * -------------------------------------------------------------------------
  */
 
+use Glpi\Cache\CacheManager;
+use Glpi\Cache\SimpleCache;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+
+global $CFG_GLPI, $PLUGIN_HOOKS;
+
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+define('GLPI_ROOT', dirname(__DIR__, 3));
+define('GLPI_LOG_DIR', GLPI_ROOT . '/files/_logs');
+
 define('TU_USER', 'glpi');
 define('TU_PASS', 'glpi');
-define('GLPI_LOG_DIR', __DIR__ . '/files/_logs');
+define('GLPI_LOG_LVL', 'DEBUG');
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
-include_once __DIR__ . '/../../../phpunit/GLPITestCase.php';
-include_once __DIR__ . '/../../../phpunit/DbTestCase.php';
+include(GLPI_ROOT . "/inc/based_config.php");
+
+if (!file_exists(GLPI_CONFIG_DIR . '/config_db.php')) {
+    die("\nConfiguration file for tests not found\n\nrun: bin/console glpi:database:install --config-dir=tests/config ...\n\n");
+}
+
+// Create subdirectories of GLPI_VAR_DIR based on defined constants
+foreach (get_defined_constants() as $constant_name => $constant_value) {
+    if (
+        preg_match('/^GLPI_[\w]+_DIR$/', $constant_name)
+        && preg_match('/^' . preg_quote(GLPI_VAR_DIR, '/') . '\//', $constant_value)
+    ) {
+        is_dir($constant_value) or mkdir($constant_value, 0755, true);
+    }
+}
+
+//init cache
+if (file_exists(GLPI_CONFIG_DIR . DIRECTORY_SEPARATOR . CacheManager::CONFIG_FILENAME)) {
+    // Use configured cache for cache tests
+    $cache_manager = new CacheManager();
+    $GLPI_CACHE = $cache_manager->getCoreCacheInstance();
+} else {
+    // Use "in-memory" cache for other tests
+    $GLPI_CACHE = new SimpleCache(new ArrayAdapter());
+}
+
+require GLPI_ROOT . '/inc/includes.php';
+include_once GLPI_ROOT . '/phpunit/GLPITestCase.php';
+include_once GLPI_ROOT . '/phpunit/DbTestCase.php';
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../setup.php';
+plugin_init_escalade();
+
+if (!file_exists(GLPI_LOG_DIR . '/php-errors.log')) {
+    file_put_contents(GLPI_LOG_DIR . '/php-errors.log', '');
+}
+
+if (!file_exists(GLPI_LOG_DIR . '/sql-errors.log')) {
+    file_put_contents(GLPI_LOG_DIR . '/sql-errors.log', '');
+}
