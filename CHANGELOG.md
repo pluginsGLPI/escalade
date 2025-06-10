ebf295f0ab510bd0fdc19520a506651641fcbce4
Langlois Gaëtan <64356364+MyvTsv@users.noreply.github.com>
2025-06-10 15:09:56 +0200

    fix: 8 warnings in php-errors.log file (#341)
    * fix 8 warnings in php-errors.log file
    
    * CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [UNRELEASE]
 
+- Fix 8 warnings in the `php-errors.log` file
+
 ## [2.9.14] - 2025-05-28
 
 - Fix Escalate timeline button execute RuleTicket
--- front/ticket.form.php
+++ front/ticket.form.php
@@ -67,8 +67,8 @@ if (isset($_POST['escalate'])) {
         $_form_object = [
             '_do_not_compute_status' => true,
         ];
-        if ($_SESSION['plugins']['escalade']['config']['ticket_last_status'] != -1) {
-            $_form_object['status'] = $_SESSION['plugins']['escalade']['config']['ticket_last_status'];
+        if ($_SESSION['glpi_plugins']['escalade']['config']['ticket_last_status'] != -1) {
+            $_form_object['status'] = $_SESSION['glpi_plugins']['escalade']['config']['ticket_last_status'];
         }
         $updates_ticket = new Ticket();
         if (
