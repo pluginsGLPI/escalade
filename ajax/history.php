8b2e084dc2d2797e59337a26207fd27216e85680
Lainow <texamaxsam61@gmail.com>
2025-06-25 15:11:32 +0200

    GLPI 11 Compatibility

--- ajax/history.php
+++ ajax/history.php
@@ -28,14 +28,11 @@
  * -------------------------------------------------------------------------
  */
 
-include("../../../inc/includes.php");
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
 
 Session::checkLoginUser();
 
-if (!isset($_REQUEST['tickets_id'])) {
-    exit;
+if (isset($_REQUEST['tickets_id'])) {
+    PluginEscaladeHistory::getHistory($_REQUEST['tickets_id']);
 }
-
-PluginEscaladeHistory::getHistory($_REQUEST['tickets_id']);
