14093d9e869dfe08263c76207b7ad6c83688e5e2
Stanislas <skita@teclib.com>
2025-07-08 15:15:45 +0200

    2.9.15 (#357)

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@
 
 use Glpi\Plugin\Hooks;
 
-define('PLUGIN_ESCALADE_VERSION', '2.9.14');
+define('PLUGIN_ESCALADE_VERSION', '2.9.15');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_ESCALADE_MIN_GLPI", "10.0.11");
