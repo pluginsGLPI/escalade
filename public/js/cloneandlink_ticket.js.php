1cbd16afd96fe38162b56ba4fd8bdc07641122de
Stanislas Kita <skita@teclib.com>
2026-02-27 14:57:05 +0100

    fix

--- public/js/cloneandlink_ticket.js.php
+++ public/js/cloneandlink_ticket.js.php
@@ -54,8 +54,9 @@ if (
       //delay the execution (ajax requestcomplete event fired before dom loading)
       setTimeout( function () {
          if ($("#cloneandlink_ticket").length > 0) { return; }
-         var duplicate_html = "<button id='cloneandlink_ticket' class='btn btn-sm btn-ghost-secondary ms-auto'"+
-                "title='{$locale_cloneandlink}'><i class='ti ti-copy me-1'></i>{$locale_clone}</button>";
+         var duplicate_html = "<button id='cloneandlink_ticket' class='btn btn-sm btn-ghost-secondary ms-auto'" +
+                "title='{$locale_cloneandlink}'><i class='ti ti-copy me-1'></i>" + '{$locale_cloneandlink}' +
+            "</button>";
 
          $("#linked_tickets-heading .accordion-button")
             .append(duplicate_html);
