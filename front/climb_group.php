3693de17e7b608e5b4cd4e277f78a52fd593238d
Langlois Gaëtan <64356364+MyvTsv@users.noreply.github.com>
2026-07-31 14:35:03 +0200

    Update front/climb_group.php
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>

--- front/climb_group.php
+++ front/climb_group.php
@@ -48,7 +48,7 @@ if (
 $tickets_id = (int) $_REQUEST['tickets_id'];
 
 $ticket = new Ticket();
-if (!$ticket->canAssign()) {
+if (!$ticket->getFromDB($tickets_id) || !$ticket->canAssign()) {
     throw new AccessDeniedHttpException();
 }
 
