1a83674360d507c4de1c8c241f57576057bda1db
Stanislas <skita@teclib.com>
2026-08-28 10:00:36 +0200

    Update front/climb_group.php
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>

--- front/climb_group.php
+++ front/climb_group.php
@@ -48,7 +48,7 @@ if (
 $tickets_id = (int) $_REQUEST['tickets_id'];
 
 $ticket = new Ticket();
-if (!$ticket->can($tickets_id, UPDATE)) {
+if (!$ticket->getFromDB($tickets_id) || !$ticket->checkEntity(true) || !$ticket->canAssign()) {
     throw new AccessDeniedHttpException();
 }
 
