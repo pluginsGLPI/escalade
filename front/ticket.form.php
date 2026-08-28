3418c9085254b9ab888daccf44505b69cc928aaf
Stanislas <skita@teclib.com>
2026-08-28 10:00:42 +0200

    Update front/ticket.form.php
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>

--- front/ticket.form.php
+++ front/ticket.form.php
@@ -40,7 +40,7 @@ if (isset($_POST['escalate'])) {
     $tickets_id = (int) $_POST['tickets_id'];
 
     $ticket = new Ticket();
-    if (!$ticket->can($tickets_id, UPDATE)) {
+    if (!$ticket->getFromDB($tickets_id) || !$ticket->checkEntity(true) || !$ticket->canAssign()) {
         throw new AccessDeniedHttpException();
     }
 
